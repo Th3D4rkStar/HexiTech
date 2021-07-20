817a018610baecc973074801f07dfb3db30bd019
BuildTools <unconfigured@null.spigotmc.org>
2021-07-20 16:55:12 +0300

    Add project files.

---
+++ HexiTech/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace HexiTech.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(DatabaseConfiguration.ConnectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ HexiTech/Data/DatabaseConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace HexiTech.Data
+{
+    public class DatabaseConfiguration
+    {
+        //TODO: Change string when testing.
+        public const string ConnectionString = 
+            @"Server=.\MSSQLSERVERNEW;Database=HexiTech;Trusted_Connection=True;Integrated Security=True;";
+    }
+}
