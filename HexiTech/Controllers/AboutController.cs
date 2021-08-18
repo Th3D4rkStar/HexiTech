27a20bb304bbb00c6b9f2d364886148dd9dc953c
Th3D4rkStar <koci_20@abv.bg>
2021-08-18 19:19:55 +0300

    Added AboutController. Finished About page.

---
+++ HexiTech/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿namespace HexiTech.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
